2ce9f6e109361942f1a4fd916bf785ebd9d52083
Shirley <shirleyzhu@college.harvard.edu>
2023-10-22 10:24:37 -0400

    add comments for readability

--- write.py
+++ write.py
@@ -7,43 +7,6 @@ notion_token = 'secret_nXAejSXmHqLfFz1TXxHgzGtFyfnwM6qR90H4GOZI6xU'
 database_id = '46db718cda374bc7aa06b551dc18be9a'
 notion_page_id = '1093311273cf47a894d7f4b9eeee992e'
 
-# Returns a mapping of book title and person name to the person's rating of the book
-# Deletes extraneous rows so that only the last rating by a person is remembered
-def delete_rows(csvreader):
-    book_person_map = {}
-    for row in csvreader:
-        book_person_pair = (normalize_name(row[0]), normalize_name(row[1]))
-        book_person_map[book_person_pair] = float(row[2])
-    return book_person_map
-
-def count_favorites(book_person_map):
-    book_favorites_map = {}
-    for key in book_person_map:
-        book = key[0]
-        if book in book_person_map:
-            if book_person_map[key] == 5: book_favorites_map[book] += 1
-        else:
-            if book_person_map[key] == 5: book_favorites_map[book] = 1
-            else: book_favorites_map[book] = 0
-    return book_favorites_map
-
-# Input: mapping of book title and person name to the person's rating of the book
-# Returns no output but writes formatted data to Notion database
-def write_data(client, book_person_map, book_favorites_map):
-    book_rating_map = {}
-    book_count_map = {}
-    for key in book_person_map:
-        book = key[0]
-        if book in book_rating_map: 
-            book_rating_map[book] += book_person_map[key]
-            book_count_map[book] += 1
-        else: 
-            book_rating_map[book] = book_person_map[key]
-            book_count_map[book] = 1
-    for book in book_rating_map:
-        row = [book, book_rating_map[book]/book_count_map[book], book_favorites_map[book]]
-        write_row(client, database_id, row[0], row[1], row[2])
-
 # Input: string
 # Returns normalized string with fixed capitalization and no extra spacing
 def normalize_name(name):
@@ -61,6 +24,31 @@ def normalize_name(name):
             normalized += str(word[0]).upper() + word[1:]
     return normalized
 
+# Input: reader to csv containing rows of book title, person name, rating of book
+# Returns a mapping of book title and person name to the person's rating of the book
+# Deletes extraneous rows so that only the last rating by a person is remembered
+def delete_rows(csvreader):
+    book_person_map = {}
+    for row in csvreader:
+        book_person_pair = (normalize_name(row[0]), normalize_name(row[1]))
+        book_person_map[book_person_pair] = float(row[2])
+    return book_person_map
+
+# Input: mapping of book title and person name to the person's rating of the book
+# Returns mapping of book title to number of people who gave the book a 5 star rating
+def count_favorites(book_person_map):
+    book_favorites_map = {}
+    for key in book_person_map:
+        book = key[0]
+        if book in book_person_map:
+            if book_person_map[key] == 5: book_favorites_map[book] += 1
+        else:
+            if book_person_map[key] == 5: book_favorites_map[book] = 1
+            else: book_favorites_map[book] = 0
+    return book_favorites_map
+
+# Input: client, database id, data to be added to database row
+# Returns no output but writes to one row to Notion database
 def write_row(client, database_id, title, rating, favorites):
     client.pages.create(
         **{
@@ -74,6 +62,23 @@ def write_row(client, database_id, title, rating, favorites):
             }
         }
     )
+
+# Input: client, mapping of book title and person name to the person's rating of the book, and mapping of book to number of favorites
+# Returns no output but writes formatted data to Notion database
+def write_data(client, book_person_map, book_favorites_map):
+    book_rating_map = {}
+    book_count_map = {}
+    for key in book_person_map:
+        book = key[0]
+        if book in book_rating_map: 
+            book_rating_map[book] += book_person_map[key]
+            book_count_map[book] += 1
+        else: 
+            book_rating_map[book] = book_person_map[key]
+            book_count_map[book] = 1
+    for book in book_rating_map:
+        row = [book, book_rating_map[book]/book_count_map[book], book_favorites_map[book]]
+        write_row(client, database_id, row[0], row[1], row[2])
 
 def main():
     client = Client(auth=notion_token)
