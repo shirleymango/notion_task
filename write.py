509dd54118eead56bc0e15ddc906a8fcabe92cb8
Shirley <shirleyzhu@college.harvard.edu>
2023-10-22 09:44:27 -0400

    create function to normalize titles

--- write.py
+++ write.py
@@ -7,6 +7,21 @@ notion_token = 'secret_nXAejSXmHqLfFz1TXxHgzGtFyfnwM6qR90H4GOZI6xU'
 database_id = '46db718cda374bc7aa06b551dc18be9a'
 notion_page_id = '1093311273cf47a894d7f4b9eeee992e'
 
+def normalize_title(title):
+    normalized = ""
+    words = title.split()
+    for word in words:
+        word = word.lower()
+        if word == "":
+            continue
+        if not normalized == "":
+            normalized += " "
+        if word == "of" or word == "to" or (word == "the" and not normalized == ""):
+            normalized += word
+        else:
+            normalized += str(word[0]).upper() + word[1:]
+    return normalized
+
 def write_row(client, database_id, title, rating, favorites):
     client.pages.create(
         **{
